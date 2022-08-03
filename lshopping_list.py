dd027321b4c6451899a01cc2856863162ecfcb2c
AgaIrma <99769729+AgaIrma@users.noreply.github.com>
2022-08-03 16:43:12 +0200

    SecondCommit

--- lshopping_list.py
+++ lshopping_list.py
@@ -13,3 +13,5 @@ for shopping_dict_key in shopping_dict:
     print(f"Ide do {shopping_dict_key}, kupuję tu następujące rzeczy: {shopping_dict_values}")
     shopping_items = shopping_items + shopping_dict_values
 print(f"W sumie kupuję { len(shopping_items)}  produktów");
+
+
