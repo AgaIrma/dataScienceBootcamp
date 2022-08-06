bcbfc47ed97ab360a24e938591f73887ae3d9dd6
Agnieszka Torba <agnieszka_torba@wp.pl>
2022-08-06 20:11:02 +0200

    Add task about modulo functions from 3 part of Kodilla

---
+++ moduloDividedKodila3.py
@@ -0,0 +1,4 @@
+numbersWhichDividesPer5 = [number for number in range(100) if number%5==0]
+print(numbersWhichDividesPer5)
+numbersMultiply    = [number*number*number for number in numbersWhichDividesPer5]
+print(numbersMultiply)
