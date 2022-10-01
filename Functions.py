1ea17d70d4d90e26f7bb4aa8c3a27e902fea7d5d
Agnieszka Torba <agnieszka_torba@wp.pl>
2022-10-01 21:34:45 +0200

    Adding first function to Jupyter notebook

---
+++ Functions.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # używamy słowa def
+
+# In[8]:
+
+
+def adding_func(a,b):
+    return a+b
+
+
+# In[ ]:
+
+
+
+
+
+# # wywołujemy funkcję poprzez podanie nazwy funkcji i użycie nawiasów okrągłych
+
+# In[10]:
+
+
+result = adding_func(4,3)
+print(f"Wynik dodawania wynosi {result}") 
+
+
+# In[6]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
